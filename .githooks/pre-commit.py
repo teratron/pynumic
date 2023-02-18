1fe87755059868610af7e7fd0c74c54e04b8f7cf
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-02-18 18:49:35 +0200

    Git hooks version.

--- .githooks/pre-commit.py
+++ .githooks/pre-commit.py
@@ -40,3 +40,29 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# def redo_config(path: str, key: str, value) -> None:
+#     with open(path) as handle:
+#         lines = handle.readlines()
+#
+#     i = 0
+#     for line in lines:
+#         _ind = line.find("=")
+#         _key = line[:_ind].strip()
+#         _val = line[_ind + 1:].strip()
+#
+#         if _ind > 0 and _key == key:
+#             version = list(map(int, _val.strip('"').split(".")))
+#             version[2] += 1
+#             __version__ = ".".join(map(str, version))
+#             lines[i] = line.replace(_val, f'"{__version__}"')
+#             break
+#
+#         i += 1
+#
+#     with open(path, "w", newline="\n") as handle:
+#         for line in lines:
+#             handle.writelines(line)
+#
+#
+# redo_config(poetry_config, "version", )
