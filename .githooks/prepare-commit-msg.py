e26d72f05bd62fff8bd92e5b148987818318bb26
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-02-18 19:03:43 +0200

    Git hooks version.

--- .githooks/prepare-commit-msg.py
+++ .githooks/prepare-commit-msg.py
@@ -3,6 +3,8 @@
 import sys
 
 commit_msg_filepath = sys.argv[1]
+
 print("Start script", commit_msg_filepath)
-with open(commit_msg_filepath, 'w') as f:
-    f.write("Start script")
+
+with open(commit_msg_filepath, "w", newline="\n") as handle:
+    handle.write("Start script")
