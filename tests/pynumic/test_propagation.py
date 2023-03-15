da632cb389a8928d6193102866a5a2a1f3ccbd7c
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-03-15 15:28:01 +0200

    pytest.

--- tests/pynumic/test_propagation.py
+++ tests/pynumic/test_propagation.py
@@ -1,14 +1,12 @@
-def test__calc_neurons():
-    assert False
+class Propagation:
+    def test__calc_neurons():
+        assert False
 
+    def test__calc_loss():
+        assert False
 
-def test__calc_loss():
-    assert False
+    def test__calc_miss():
+        assert False
 
-
-def test__calc_miss():
-    assert False
-
-
-def test__update_weights():
-    assert False
+    def test__update_weights():
+        assert False
